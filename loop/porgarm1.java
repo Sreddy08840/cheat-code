327b22887bff035195184d42cfb6814b1a1a2882
santosh <sreddy08840@gmail.com>
2026-03-25 16:41:02 +0530

    the loop synstax

---
+++ loop/porgarm1.java
@@ -0,0 +1,14 @@
+class poragrma{
+    public static void main(String[] args) {
+        // for(int i=1;i<=5;i++)
+        // {
+        //     System.out.println("Hello World");
+        // }
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
