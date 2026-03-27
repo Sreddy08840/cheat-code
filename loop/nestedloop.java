594fc0c970a4e33e286176f2b20f33407022c960
santosh <sreddy08840@gmail.com>
2026-03-27 16:47:14 +0530

    nested arrray

--- loop/nestedloop.java
+++ loop/nestedloop.java
@@ -2,7 +2,7 @@ class nestedloop{
     public static void main(String[] args){
         for(int i =0;i<3;i++){
             for(int j=0;j<5;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
