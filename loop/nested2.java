38063075c5791b14af00ef4923c6138f8c9a50c9
santosh <sreddy08840@gmail.com>
2026-03-27 17:01:01 +0530

    nested arary clear example

---
+++ loop/nested2.java
@@ -0,0 +1,10 @@
+public class nested2 {
+ public static void main(String[] args){
+     for(int i=0;i<3;i++){
+         for(int j=0;j<5;j++){
+             System.out.println("i"+i+"+j"+j+" ");
+         }
+         System.out.println();
+     }
+ }
+}
