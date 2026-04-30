c416c64643d7ff54f1a03e5b85ef11f9870bb932
Santosh <sreddy08840@gmail.com>
2026-04-30 16:39:21 +0530

    Fix loop structure in example1.java

--- example1.java
+++ example1.java
@@ -8,8 +8,7 @@ public class example1 {
 		
 		int n=scan.nextInt();
 		int[] ar =new int[n];
-	
-		for(int i=0;i<ar.length;i++) {
+	   for(int i=0;i<ar.length;i++) {
 		ar[i]=scan.nextInt();	
 		}
 		HashSet<Integer> lh=new HashSet<Integer>();
