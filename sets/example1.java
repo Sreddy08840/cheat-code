a48a61b3e80826fc4735f392f3e14f439dec6356
Santosh <sreddy08840@gmail.com>
2026-05-01 12:35:59 +0530

    Fix typo in print statement and variable declaration

--- sets/example1.java
+++ sets/example1.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 public class example1 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		
-		System.out.print("Enter n: ");
-		int n=scan.nextInt();
+		Sylstem.out.print("Enter n: ");
+	l	int n=scan.nextInt();
 		if (n <= 0) {
 		    System.out.println("Invalid n");
 		    return;
